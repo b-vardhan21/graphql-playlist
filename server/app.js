aea3f296f5eaf408f9ad2eaf79155d24e2b359ad
vardhan barishetti <67987365+b-vardhan21@users.noreply.github.com>
2023-10-14 16:02:13 -0400

    root query

--- server/app.js
+++ server/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const {graphqlHTTP} = require('express-graphql');
 
+const schema = require('./schema/schema');
 const app = express();
 
 
 app.use('/graphql', graphqlHTTP({
 
-
-
+    schema
 
 }));
 //tell our app to listen to specific port
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,12 @@
 const graphql = require('graphql');
 
-const {GraphQlObjectType, GraphQLString} = graphql;
+const {GraphQlObjectType, GraphQLString, GraphQlSchema} = graphql;
 
 
 
 //here we define our first object type on this graph and it is book type
 //it called book and it has some fields 
 //we wrapped all the fields inside a function which returns object 
-//
-//
 
 const BookType = new GraphQlObjectType({ 
     name: 'Book',
@@ -20,3 +18,33 @@ const BookType = new GraphQlObjectType({
 
 
 });
+
+
+
+/*
+when some queries this "BookType" for particular book we expect them to pass some arguments, because we dont know
+what book they want is it book id 1, 2, 3, 4 etc
+in front end looks like this
+book(id:'123'){
+    name 
+    genre
+}
+*/ 
+const RootQuery = GraphQlObjectType({
+    name : 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                //code to get data from db
+            }
+
+        }
+    }
+});
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
